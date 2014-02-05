540fdb41827bf3370733622f17be33fa0c2c5753
Govinda Fichtner <govinda.fichtner@googlemail.com>
2014-02-05 22:17:52 +0100

    fix keyboard and locale settings

--- vagrant/ubuntu_1310/cookbooks/dev_box/recipes/tidy_up.rb
+++ vagrant/ubuntu_1310/cookbooks/dev_box/recipes/tidy_up.rb
@@ -1,6 +1,11 @@
-execute "set-keyboard" do
-  command %Q(setxkbmap -model pc105 -layout de -variant basic)
+cookbook_file "keyboard" do
+  path "/etc/default/keyboard"
 end
+
+cookbook_file "locale" do
+  path "/etc/default/locale"
+end
+
 package "fonts-inconsolata"
 
 # remove cache
